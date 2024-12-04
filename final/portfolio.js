070d5190106b75c7ae6d6a4671e1e334fdd9beda
aubrey888 <auha6209@colorado.edu>
2024-12-04 09:26:58 -0700

    trying to fix together apart

--- final/portfolio.js
+++ final/portfolio.js
@@ -13,16 +13,24 @@ fetch('../final/projects.json')
 // first project - candyland / c++ part of portfolio
 // div for each part of the json file + different displays 
 
-function parseData(data){
-    for(let i=0; i<data.projects.length; i++){
-    document.getElementById("projects").innerHTML += `<a href = /final/${data.projects[i].subdomain}.html">
-    <div class="row project" id="${data.projects[i].subdomain}">
-    
-        <div class="projimg"> <img src="https://aubrey888.github.io/images/chick.png"> </div>
-        <div class="description"> <h2>${data.projects[i].name}</h2><p class="subtitle">${data.projects[i].subtitle}</p>
-        <p>${data.projects[i].abstract}</p></div></div></a>`;
+function parseData(data) {
+    for (let i = 0; i < data.projects.length; i++) {
+        document.getElementById("projects").innerHTML += `
+        <a href="./final/${data.projects[i].subdomain}.html">
+            <div class="row project" id="${data.projects[i].subdomain}">
+                <div class="projimg">
+                    <img src="https://aubrey888.github.io/images/chick.png">
+                </div>
+                <div class="description">
+                    <h2>${data.projects[i].name}</h2>
+                    <p class="subtitle">${data.projects[i].subtitle}</p>
+                    <p>${data.projects[i].abstract}</p>
+                </div>
+            </div>
+        </a>`;
     }
 }
+
 
 for(b of document.querySelectorAll("#buttons button")) {
     b.addEventListener("click", e=> {
